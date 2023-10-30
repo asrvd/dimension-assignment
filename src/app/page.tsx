8597bc52e58a1533c783b8618c748189c275be00
asrvd <asheeshh@duck.com>
2023-10-30 22:23:33 +0530

    chore: fix ssr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,17 +11,23 @@ async function getTasks() {
   return tasks;
 }
 
-export default async function Home() {
+async function Tasks() {
   const tasks = await getTasks();
+  return (
+    <div className="flex flex-col gap-2">
+      {tasks.map((task) => (
+        <TaskCard key={task.id} task={task} />
+      ))}
+    </div>
+  );
+}
+
+export default function Home() {
   return (
     <main className="flex min-h-screen relative bg-white min-w-screen flex-col items-center justify-between px-6 lg:px-24 pb-6 lg:pb-24">
       <section className="flex flex-col lg:w-2/3 w-full h-full relative bg-white">
         <TaskModal />
-        <div className="flex flex-col gap-2">
-          {tasks.map((task) => (
-            <TaskCard key={task.id} task={task} />
-          ))}
-        </div>
+        <Tasks />
       </section>
     </main>
   );
